779f3a149076f07d2c519854f0172f071a449ef3
yunhyel22 <yunhyel22@gmail.com>
2024-08-06 02:14:35 +0900

    fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const lists = document.querySelectorAll('#skillList li');
 lists.forEach((el, key) => {
     const name = $(el).text();
     $(el).html(`
-        <div class="box" style="background-image:url('./images/${name.replace(/\s+/g, '').toLowerCase()}.png')"></div>
+        <div class="box" title="" tabindex="-1" style="background-image:url('./images/${name.replace(/\s+/g, '').toLowerCase()}.png')"></div>
         <p>${name}</p>
     `);
 })
@@ -14,17 +14,18 @@ $('*[data-copy]').on('click', function() {
     notify(`
         클립보드에 제 이메일이 복사되었습니다.
         메일로 연락 주세요 :-)
-    `);
+    `, $(this));
 })
 
 
-function notify(msg) {
+function notify(msg, prevT) {
     const target = $('#notify');
     target.find('p').html(msg);
-    target.fadeIn(300);
+    target.fadeIn(300).focus();
     setTimeout(() => {
         target.fadeOut(300, () => {
             target.find('p').html('');
+            prevT.focus();
         });
     }, 3000);
 }
